da00bc9d10820625142a0166249359fb3765b947
WaltD-007 <ab@barclaysimpson.com>
2026-04-05 17:07:04 +0100

    Export Google Jobs adapter

--- src/vacancysoft/adapters/__init__.py
+++ src/vacancysoft/adapters/__init__.py
@@ -1,6 +1,7 @@
 from vacancysoft.adapters.adzuna import AdzunaAdapter
 from vacancysoft.adapters.eightfold import EightfoldAdapter
 from vacancysoft.adapters.generic_browser import GenericBrowserAdapter
+from vacancysoft.adapters.google_jobs import GoogleJobsAdapter
 from vacancysoft.adapters.greenhouse import GreenhouseAdapter
 from vacancysoft.adapters.workable import WorkableAdapter
 from vacancysoft.adapters.workday import WorkdayAdapter, derive_workday_candidate_endpoints
@@ -9,6 +10,7 @@ __all__ = [
     "AdzunaAdapter",
     "EightfoldAdapter",
     "GenericBrowserAdapter",
+    "GoogleJobsAdapter",
     "GreenhouseAdapter",
     "WorkableAdapter",
     "WorkdayAdapter",
