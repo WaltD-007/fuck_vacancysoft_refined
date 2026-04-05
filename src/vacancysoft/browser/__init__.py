f1c23a291d62b8e858b35337cb18f74988cb9bc5
WaltD-007 <ab@barclaysimpson.com>
2026-04-05 19:18:48 +0100

    Add shared browser utilities package

---
+++ src/vacancysoft/browser/__init__.py
@@ -0,0 +1,3 @@
+from vacancysoft.browser.session import browser_session, browserless_concurrency_limit, uses_browserless
+
+__all__ = ["browser_session", "browserless_concurrency_limit", "uses_browserless"]
